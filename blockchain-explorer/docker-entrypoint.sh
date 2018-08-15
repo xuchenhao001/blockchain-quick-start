a08d9ce14f93f3eb0ca8dbc867a64e009a2c7f80
xuchenhao001 <xu.chenhao@hotmail.com>
2018-08-15 13:47:27 +0800

    explorer workaround for fabric 1.2

--- blockchain-explorer/docker-entrypoint.sh
+++ blockchain-explorer/docker-entrypoint.sh
@@ -4,7 +4,7 @@ function modifyNetwork() {
 	if [ -f "/docker-entrypoint-initdb.d/config.json" ]; then
 		echo "File \"/docker-entrypoint-initdb.d/config.json\" exists."
 		echo "Replace \"app/platform/fabric/config.json\" with the new one."
-		cp /docker-entrypoint-initdb.d/config.json app/platform/fabric/config.json
+		cp /docker-entrypoint-initdb.d/config.json /blockchain-explorer/app/platform/fabric/config.json
 	fi
 }
 
@@ -12,16 +12,16 @@ function modifyDB() {
 	if [ -f "/docker-entrypoint-initdb.d/config.json" ]; then
 		echo "File \"/docker-entrypoint-initdb.d/pgconfig.json\" exists."
 		echo "Replace \"app/persistence/postgreSQL/db/pgconfig.json\" with the new one."
-		cp /docker-entrypoint-initdb.d/pgconfig.json app/persistence/postgreSQL/db/pgconfig.json
+		cp /docker-entrypoint-initdb.d/pgconfig.json /blockchain-explorer/app/persistence/postgreSQL/db/pgconfig.json
 	fi
 }
 
 function startApp() {
 	rm -rf /tmp/fabric-client-kvs_peerOrg*
+        cd /blockchain-explorer/
 	node main.js
 }
 
-cd blockchain-explorer/
 modifyNetwork
 modifyDB
 startApp
