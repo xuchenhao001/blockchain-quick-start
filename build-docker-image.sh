3068399a37d98490780413287dc22caefdec8eeb
xuchenhao001 <xu.chenhao@qq.com>
2019-01-31 16:45:27 +0800

    build docker image with git hash now

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+set -x
+
 rm -rf ./tmp
 
 # Prepare binary
 docker run --rm -v $PWD/tmp/:/var/tmp/ hyperledger/fabric-tools:1.3.0 bash -c "cp /usr/local/bin/configtx* /var/tmp/"
 
+# Prepare git hash
+GITHASH=$(git rev-parse --short HEAD)
+
 # Build docker image
 cp -r app.js bin docker-entrypoint.sh Dockerfile package.json package-lock.json public routes chaincode patch ./tmp
-docker build -t blockchain-rest-server:1.0.0 ./tmp
+docker build -t blockchain-rest-server:$GITHASH ./tmp
 rm -rf ./tmp
 
