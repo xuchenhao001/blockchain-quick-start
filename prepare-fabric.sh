423ed5c2cc6d1bbf8e2db46bec74ef668838d4b0
xuchenhao001 <xu.chenhao@qq.com>
2019-02-03 10:23:44 +0000

    upgrade fabric to v1.4

--- prepare-fabric.sh
+++ prepare-fabric.sh
@@ -6,11 +6,11 @@
 #
 
 # if version not passed in, default to latest released version
-export VERSION=1.3.0
+export VERSION=1.4.0
 # if ca version not passed in, default to latest released version
 export CA_VERSION=$VERSION
 # current version of thirdparty images (couchdb, kafka and zookeeper) released
-export THIRDPARTY_IMAGE_VERSION=0.4.13
+export THIRDPARTY_IMAGE_VERSION=0.4.14
 export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
 export MARCH=$(uname -m)
 
