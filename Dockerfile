c39a2f229679a9323be96864e3899377dc7815a4
xuchenhao001 <dedog.xu@gmail.com>
2019-05-10 15:02:57 +0800

    fix bug

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10.15.1
+FROM node:10.15.3
 
 COPY . /blockchain-quick-start/
 
--- build-docker-image.sh
+++ build-docker-image.sh
@@ -12,7 +12,7 @@ docker run --rm -v $PWD/tmp/:/var/tmp/ hyperledger/fabric-tools:${VERSION} bash 
 GITHASH=$(git rev-parse --short HEAD)
 
 # Build docker image
-cp -r app.js bin docker-entrypoint.sh Dockerfile package.json package-lock.json public routes chaincode patch ./tmp
+cp -r app.js bin docker-entrypoint.sh Dockerfile package.json package-lock.json public routes chaincode ./tmp
 docker build -t blockchain-rest-server:$GITHASH ./tmp
 rm -rf ./tmp
 
